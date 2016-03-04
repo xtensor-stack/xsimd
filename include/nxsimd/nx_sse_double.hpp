20f4d806934d1b0aeb911e5d254b1ae5cd936775
jmabille <johan.mabille@gmail.com>
2016-03-04 01:03:29 +0100

    license update

--- include/nxsimd/nx_sse_double.hpp
+++ include/nxsimd/nx_sse_double.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012 - 2016 Johan Mabille
+// Copyright (c) 2016 Johan Mabille
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
