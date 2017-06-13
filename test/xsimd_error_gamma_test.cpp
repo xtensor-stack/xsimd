556acecfe470b42688c295b8dfed4dcdd573766b
Johan Mabille <johan.mabille@gmail.com>
2017-06-13 16:23:30 +0200

    erf and erfc functions

---
+++ test/xsimd_error_gamma_test.cpp
@@ -0,0 +1,59 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include <iostream>
+#include <fstream>
+
+#include "gtest/gtest.h"
+
+#include "xsimd/memory/xsimd_aligned_allocator.hpp"
+#include "xsimd/types/xsimd_types_include.hpp"
+#include "xsimd/math/xsimd_error_gamma.hpp"
+#include "xsimd_error_gamma_test.hpp"
+
+namespace xsimd
+{
+    template <class T, size_t N, size_t A>
+    bool test_error_gamma(std::ostream& out, const std::string& name)
+    {
+        simd_error_gamma_tester<T, N, A> tester(name);
+        return test_simd_error_gamma(out, tester);
+    }
+}
+
+#if XSIMD_X86_INSTR_SET >= XSIMD_X86_SSE2_VERSION
+TEST(xsimd, sse_float_error_gamma)
+{
+    std::ofstream out("log/sse_float_error_gamma.log", std::ios_base::out);
+    bool res = xsimd::test_error_gamma<float, 4, 16>(out, "sse float");
+    EXPECT_TRUE(res);
+}
+
+TEST(xsimd, sse_double_error_gamma)
+{
+    std::ofstream out("log/sse_double_error_gamma.log", std::ios_base::out);
+    bool res = xsimd::test_error_gamma<double, 2, 16>(out, "sse double");
+    EXPECT_TRUE(res);
+}
+#endif
+
+#if XSIMD_X86_INSTR_SET >= XSIMD_X86_AVX_VERSION
+TEST(xsimd, avx_float_error_gamma)
+{
+    std::ofstream out("log/avx_float_error_gamma.log", std::ios_base::out);
+    bool res = xsimd::test_error_gamma<float, 8, 32>(out, "avx float");
+    EXPECT_TRUE(res);
+}
+
+TEST(xsimd, avx_double_error_gamma)
+{
+    std::ofstream out("log/avx_double_exponential.log", std::ios_base::out);
+    bool res = xsimd::test_error_gamma<double, 4, 32>(out, "avx double");
+    EXPECT_TRUE(res);
+}
+#endif
---
+++ test/xsimd_error_gamma_test.hpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XSIMD_ERROR_GAMMA_TEST_HPP
+#define XSIMD_ERROR_GAMMA_TEST_HPP
+
+#include "xsimd_tester.hpp"
+#include "xsimd_test_utils.hpp"
+
+namespace xsimd
+{
+
+    template <class T, std::size_t N, std::size_t A>
+    struct simd_error_gamma_tester : simd_tester<T, N, A>
+    {
+        using base_type = simd_tester<T, N, A>;
+        using vector_type = typename base_type::vector_type;
+        using value_type = typename base_type::value_type;
+        using res_type = typename base_type::res_type;
+
+        std::string name;
+
+        res_type input;
+        res_type erf_res;
+        res_type erfc_res;
+
+        simd_error_gamma_tester(const std::string& n);
+    };
+
+    template <class T, std::size_t N, std::size_t A>
+    simd_error_gamma_tester<T, N, A>::simd_error_gamma_tester(const std::string& n)
+        : name(n)
+    {
+        size_t nb_input = N * 10000;
+        input.resize(nb_input);
+        erf_res.resize(nb_input);
+        erfc_res.resize(nb_input);
+        for (size_t i = 0; i < nb_input; ++i)
+        {
+            input[i] = value_type(-1.5) + i * value_type(3) / nb_input;
+            erf_res[i] = std::erf(input[i]);
+            erfc_res[i] = std::erfc(input[i]);
+        }
+    }
+
+    template <class T>
+    bool test_simd_error_gamma(std::ostream& out, T& tester)
+    {
+        using tester_type = T;
+        using vector_type = typename tester_type::vector_type;
+        using value_type = typename tester_type::value_type;
+        using res_type = typename tester_type::res_type;
+
+        vector_type input;
+        vector_type vres;
+        res_type res(tester.input.size());
+
+        bool success = true;
+        bool tmp_success = true;
+
+        std::string val_type = value_type_name<vector_type>();
+        std::string shift = std::string(val_type.size(), '-');
+        std::string name = tester.name;
+        std::string name_shift = std::string(name.size(), '-');
+        std::string dash(8, '-');
+        std::string space(8, ' ');
+
+        out << dash << name_shift << '-' << shift << dash << std::endl;
+        out << space << name << " " << val_type << std::endl;
+        out << dash << name_shift << '-' << shift << dash << std::endl << std::endl;
+
+        out << "erf   : ";
+        for (size_t i = 0; i < tester.input.size(); i += tester.size)
+        {
+            detail::load_vec(input, tester.input, i);
+            vres = erf(input);
+            detail::store_vec(vres, res, i);
+        }
+        tmp_success = check_almost_equal(res, tester.erf_res, out);
+        success = success && tmp_success;
+
+        out << "erfc  : ";
+        for (size_t i = 0; i < tester.input.size(); i += tester.size)
+        {
+            detail::load_vec(input, tester.input, i);
+            vres = erfc(input);
+            detail::store_vec(vres, res, i);
+        }
+        tmp_success = check_almost_equal(res, tester.erfc_res, out);
+        success = success && tmp_success;
+
+        return success;
+    }
+
+}
+
+#endif
