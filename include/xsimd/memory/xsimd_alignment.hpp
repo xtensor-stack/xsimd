e846c8e35c3590cf2820417294316b0901721421
Johan Mabille <johan.mabille@gmail.com>
2017-09-18 23:44:11 +0200

    alignment metafunctions

---
+++ include/xsimd/memory/xsimd_alignment.hpp
@@ -0,0 +1,81 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XSIMD_ALIGNMENT_HPP
+#define XSIMD_ALIGNMENT_HPP
+
+#include "../config/xsimd_align.hpp"
+#include "xsimd_aligned_allocator.hpp"
+
+namespace xsimd
+{
+    /**
+    * @struct aligned_mode
+    * @brief tag for load and store of aligned memory.
+    */
+    struct aligned_mode
+    {
+    };
+
+    /**
+    * @struct unaligned_mode
+    * @brief tag for load and store of unaligned memory.
+    */
+    struct unaligned_mode
+    {
+    };
+
+    /***********************
+     * Allocator alignment *
+     ***********************/
+
+    template <class A>
+    struct allocator_alignment
+    {
+        using type = unaligned_mode;
+    };
+
+#ifdef XSIMD_X86_INSTR_SET_AVAILABLE
+    template <class T>
+    struct allocator_alignment<aligned_allocator<T, XSIMD_DEFAULT_ALIGNMENT>>
+    {
+        using type = aligned_mode;
+    };
+#endif
+
+    template <class A>
+    using allocator_alignment_t = typename allocator_alignment<A>::type;
+
+    /***********************
+     * container alignment *
+     ***********************/
+
+    namespace detail
+    {
+        template <class T>
+        using void_t = void;
+    }
+
+    template <class C, class = detail::void_t<void>>
+    struct container_alignment
+    {
+        using type = unaligned_mode;
+    };
+
+    template <class C>
+    struct container_alignment<C, detail::void_t<typename C::allocator_type>>
+    {
+        using type = allocator_alignment_t<typename C::allocator_type>;
+    };
+
+    template <class C>
+    using container_alignment_t = typename container_alignment<C>::type;
+
+}
+
+#endif
---
+++ test/xsimd_memory_test.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include <vector>
+#include <type_traits>
+
+#include "gtest/gtest.h"
+
+#include "xsimd/memory/xsimd_alignment.hpp"
+
+namespace xsimd
+{
+    struct mock_container {};
+
+    TEST(xsimd, alignment)
+    {
+        using u_vector_type = std::vector<double>;
+        using a_vector_type = std::vector<double, aligned_allocator<double, XSIMD_DEFAULT_ALIGNMENT>>;
+
+        using u_vector_align = container_alignment_t<u_vector_type>;
+        using a_vector_align = container_alignment_t<a_vector_type>;
+        using mock_align = container_alignment_t<mock_container>;
+
+        EXPECT_TRUE((std::is_same<u_vector_align, unaligned_mode>::value));
+        EXPECT_TRUE((std::is_same<a_vector_align, aligned_mode>::value));
+        EXPECT_TRUE((std::is_same<mock_align, unaligned_mode>::value));
+    }
+}
