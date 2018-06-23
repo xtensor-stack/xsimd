98f657f8231e1c8c7efe079f1c358ba03d9a186a
Wolf Vollprecht <w.vollprecht@gmail.com>
2018-06-23 12:49:25 +0200

    formatting

--- include/xsimd/routines/builder.hpp
+++ include/xsimd/routines/builder.hpp
@@ -13,25 +13,25 @@
 
 namespace xsimd
 {
-	template <class T, std::size_t N>
-	inline void arange(batch<T, N>& batch, T start = 0, T step = 1)
-	{
-		T val = start;
-		alignas(32) T xrange[N];
-		for (auto it = std::begin(xrange); it != std::end(xrange); ++it)
-		{
-			*it = val;
-			val += step;
-		}
-		xsimd::load_aligned(xrange, batch);
-	}
+    template <class T, std::size_t N>
+    inline void arange(batch<T, N>& batch, T start = 0, T step = 1)
+    {
+        T val = start;
+        alignas(XSIMD_DEFAULT_ALIGNMENT) T xrange[N];
+        for (auto it = std::begin(xrange); it != std::end(xrange); ++it)
+        {
+            *it = val;
+            val += step;
+        }
+        xsimd::load_aligned(xrange, batch);
+    }
 
-	template <class I, class T>
-	inline void arange(I begin, I end, T start, T step)
-	{
-		using value_type = std::decay_t<decltype(*begin)>;
-		using traits = simd_traits<value_type>;
-		using batch_type = typename traits::type;
+    template <class I, class T>
+    inline void arange(I begin, I end, T start, T step)
+    {
+        using value_type = std::decay_t<decltype(*begin)>;
+        using traits = simd_traits<value_type>;
+        using batch_type = typename traits::type;
 
         std::size_t size = static_cast<std::size_t>(std::distance(begin, end));
         std::size_t simd_size = traits::size;
@@ -39,39 +39,39 @@ namespace xsimd
         value_type* ptr_begin = &(*begin);
         value_type* ptr_end = &(*end);
 
-		std::size_t align_begin = xsimd::get_alignment_offset(ptr_begin, size, simd_size);
-		std::size_t align_end = align_begin + ((size - align_begin) & ~(simd_size - 1));
+        std::size_t align_begin = xsimd::get_alignment_offset(ptr_begin, size, simd_size);
+        std::size_t align_end = align_begin + ((size - align_begin) & ~(simd_size - 1));
 
-		for (std::size_t i = 0; i < align_begin; ++i)
-		{
-		    *ptr_begin = start;
-		    ++ptr_begin;
-		    start += step;
-		}
+        for (std::size_t i = 0; i < align_begin; ++i)
+        {
+            *ptr_begin = start;
+            ++ptr_begin;
+            start += step;
+        }
 
-		batch_type brange, bstep(step * traits::size);
-		arange(brange, start, step);
+        batch_type brange, bstep(step * traits::size);
+        arange(brange, start, step);
 
-		for (std::size_t i = align_begin; i < align_end; i += simd_size)
-		{
-			xsimd::store_aligned(ptr_begin, brange);
-			brange += bstep;
-			ptr_begin += simd_size;
-		}
+        for (std::size_t i = align_begin; i < align_end; i += simd_size)
+        {
+            xsimd::store_aligned(ptr_begin, brange);
+            brange += bstep;
+            ptr_begin += simd_size;
+        }
 
-		start = *(ptr_begin - 1) + step;
-		for (std::size_t i = align_end; i < size; ++i)
-		{
-			*ptr_begin = start;
-			++ptr_begin;
-			start += step;
-		}
-	}
+        start = *(ptr_begin - 1) + step;
+        for (std::size_t i = align_end; i < size; ++i)
+        {
+            *ptr_begin = start;
+            ++ptr_begin;
+            start += step;
+        }
+    }
 
-	template <class I>
-	inline void arange(I begin, I end)
-	{
-		arange(begin, end, 0, 1);
-	}
+    template <class I>
+    inline void arange(I begin, I end)
+    {
+        arange(begin, end, 0, 1);
+    }
 }
 #endif
