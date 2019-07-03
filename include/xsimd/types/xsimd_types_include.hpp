002e7e9c2534b3a8a81b2d43ddf5536d7236d742
Johan Mabille <johan.mabille@gmail.com>
2019-07-03 13:54:39 +0200

    Added the possibility to force instruction set via cmake command line

--- include/xsimd/types/xsimd_types_include.hpp
+++ include/xsimd/types/xsimd_types_include.hpp
@@ -69,9 +69,9 @@
 
 #if XSIMD_ARM_INSTR_SET == XSIMD_VERSION_NUMBER_NOT_AVAILABLE && XSIMD_X86_INSTR_SET == XSIMD_VERSION_NUMBER_NOT_AVAILABLE
 	#if defined(XSIMD_ENABLE_FALLBACK)
-		#warning "No SIMD instructions enabled, using fallback mode."
+		#warning "No SIMD instructions set detected, using fallback mode."
 	#else
-		#warning "Please compile with SIMD instructions enabled or activate the fallback mode. (e.g. for x86 -march=native or for ARM -mfpu=neon)"
+		#warning "No SIMD instructions set detected, please enable SIMD instructions or activate the fallback mode. (e.g. for x86 -march=native or for ARM -mfpu=neon)"
 	#endif
 #endif
 
