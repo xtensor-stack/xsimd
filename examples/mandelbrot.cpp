86bc4c74897301e76dbb24e8be3d9c718a9ef160
mattn <mattn.jp@gmail.com>
2021-04-20 05:34:10 +0900

    Fix segmentation fault on mingw (#457)
    Fix segmentation fault on mingw
    
    Instead of using a stack-allocated variable, use an heap-allocated one.

--- examples/mandelbrot.cpp
+++ examples/mandelbrot.cpp
@@ -15,6 +15,7 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "pico_bench.hpp"
 
@@ -228,7 +229,7 @@ int main()
     const float y1            = 1;
     const int maxIters        = 256;
 
-    alignas(64) std::array<int, width * height> buf;
+    std::vector<int, xsimd::aligned_allocator<int, XSIMD_DEFAULT_ALIGNMENT>> buf(width * height);
 
     auto bencher = pico_bench::Benchmarker<milliseconds>{64, seconds{10}};
 
