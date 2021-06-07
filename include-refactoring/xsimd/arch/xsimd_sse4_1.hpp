26b79d3df2383f05dc7b8dd52e16784a0cccbdf0
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-06-08 00:01:04 +0200

    + select and batch_bool_constant

--- include-refactoring/xsimd/arch/xsimd_sse4_1.hpp
+++ include-refactoring/xsimd/arch/xsimd_sse4_1.hpp
@@ -41,6 +41,15 @@ namespace xsimd {
       return _mm_blendv_pd(false_br, true_br, cond);
     }
 
+    template<class A, bool... Values> batch<float, A> select(batch_bool_constant<batch<float, A>, Values...> const& , batch<float, A> const& true_br, batch<float, A> const& false_br, requires<sse4_1>) {
+      constexpr int mask = batch_bool_constant<batch<float, A>, Values...>::mask();
+      return _mm_blend_ps(false_br, true_br, mask);
+    }
+    template<class A, bool... Values> batch<double, A> select(batch_bool_constant<batch<double, A>, Values...> const& , batch<double, A> const& true_br, batch<double, A> const& false_br, requires<sse4_1>) {
+      constexpr int mask = batch_bool_constant<batch<double, A>, Values...>::mask();
+      return _mm_blend_pd(false_br, true_br, mask);
+    }
+
 
     // trunc
     template<class A> batch<float, A> trunc(batch<float, A> const& self, requires<sse4_1>) {
