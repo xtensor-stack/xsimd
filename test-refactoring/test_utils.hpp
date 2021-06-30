a9cadbc09a9e2e90c57093d6e142cb3ef0871afe
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-06-30 22:17:18 +0200

    Supporttest_complex_exponential and test_complex_trigonometric

--- test-refactoring/test_utils.hpp
+++ test-refactoring/test_utils.hpp
@@ -54,7 +54,7 @@ namespace xsimd
     inline bool operator==(const batch<T, A>& lhs, const std::array<T, N>& rhs)
     {
         std::array<T, N> tmp;
-        store(tmp.data(), lhs);
+        lhs.store_unaligned(tmp.data());
         return tmp == rhs;
     }
 
