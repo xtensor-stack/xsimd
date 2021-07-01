75329f9075a75193ba272c00e64734250feab1a9
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-07-01 22:59:23 +0200

    final sse touch

--- include-refactoring/xsimd/xsimd.hpp
+++ include-refactoring/xsimd/xsimd.hpp
@@ -1,6 +1,13 @@
 #ifndef XSIMD_HPP
 #define XSIMD_HPP
 
+#if defined(__GNUC__)
+#define XSIMD_NO_DISCARD __attribute__((warn_unused_result))
+#else
+#define XSIMD_NO_DISCARD
+#endif
+
+
 #include "types/xsimd_batch.hpp"
 #include "types/xsimd_batch_constant.hpp"
 #include "types/xsimd_api.hpp"
