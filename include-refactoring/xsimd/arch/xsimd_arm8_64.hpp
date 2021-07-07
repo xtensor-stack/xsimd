e632cd0b957170acbaa70d32307d8f57dd24cb0f
Johan Mabille <johan.mabille@gmail.com>
2021-07-07 17:01:00 +0200

    Implemented neg for arm8_64

--- include-refactoring/xsimd/arch/xsimd_arm8_64.hpp
+++ include-refactoring/xsimd/arch/xsimd_arm8_64.hpp
@@ -10,6 +10,10 @@ namespace xsimd
     {
         using namespace types;
 
+        /********
+         * load *
+         ********/
+
         template <class A>
         batch<double, A> load_aligned(double const* src, convert<double>, requires<arm8_64>)
         {
@@ -22,6 +26,10 @@ namespace xsimd
             return load_aligned<A>(src, convert<double>{}, A{});
         }
 
+        /*********
+         * store *
+         *********/
+
         template <class A>
         void store_aligned(double* dst, batch<double, A> const& src, requires<arm8_64>)
         {
@@ -32,6 +40,16 @@ namespace xsimd
         void store_unaligned(double* dst, batch<double, A> const& src, requires<arm8_64>)
         {
             store_aligned<A>(dst, src, A{});
+        }
+
+        /*******
+         * neg *
+         *******/
+
+        template <class A>
+        batch<double, A> neg(batch<double, A> const& rhs, requires<arm8_64>)
+        {
+            return vnegq_f64(rhs);
         }
     }
 }
