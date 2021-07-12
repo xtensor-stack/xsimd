52ded96327200cb9fe35fe1e3e82e91e48bafa76
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-07-12 23:36:48 +0200

    generic compare operator

--- include-refactoring/xsimd/arch/xsimd_avx512f.hpp
+++ include-refactoring/xsimd/arch/xsimd_avx512f.hpp
@@ -50,6 +50,67 @@ namespace xsimd {
     }
     namespace detail {
 
+
+inline uint64_t morton_split(uint32_t a){
+	uint64_t x = a;
+	x = (x | x << 32) & 0x00000000FFFFFFFF;
+	x = (x | x << 16) & 0x0000FFFF0000FFFF;
+	x = (x | x << 8) & 0x00FF00FF00FF00FF;
+	x = (x | x << 4) & 0x0F0F0F0F0F0F0F0F;
+	x = (x | x << 2) & 0x3333333333333333;
+	x = (x | x << 1) & 0x5555555555555555;
+	return x;
+}
+
+inline uint64_t morton(uint32_t x, uint32_t y){
+	return morton_split(x) | (morton_split(y) << 1);
+}
+
+    inline uint32_t morton(uint16_t x, uint16_t y) {
+
+      static const unsigned short MortonTable256[256] = 
+      {
+        0x0000, 0x0001, 0x0004, 0x0005, 0x0010, 0x0011, 0x0014, 0x0015, 
+        0x0040, 0x0041, 0x0044, 0x0045, 0x0050, 0x0051, 0x0054, 0x0055, 
+        0x0100, 0x0101, 0x0104, 0x0105, 0x0110, 0x0111, 0x0114, 0x0115, 
+        0x0140, 0x0141, 0x0144, 0x0145, 0x0150, 0x0151, 0x0154, 0x0155, 
+        0x0400, 0x0401, 0x0404, 0x0405, 0x0410, 0x0411, 0x0414, 0x0415, 
+        0x0440, 0x0441, 0x0444, 0x0445, 0x0450, 0x0451, 0x0454, 0x0455, 
+        0x0500, 0x0501, 0x0504, 0x0505, 0x0510, 0x0511, 0x0514, 0x0515, 
+        0x0540, 0x0541, 0x0544, 0x0545, 0x0550, 0x0551, 0x0554, 0x0555, 
+        0x1000, 0x1001, 0x1004, 0x1005, 0x1010, 0x1011, 0x1014, 0x1015, 
+        0x1040, 0x1041, 0x1044, 0x1045, 0x1050, 0x1051, 0x1054, 0x1055, 
+        0x1100, 0x1101, 0x1104, 0x1105, 0x1110, 0x1111, 0x1114, 0x1115, 
+        0x1140, 0x1141, 0x1144, 0x1145, 0x1150, 0x1151, 0x1154, 0x1155, 
+        0x1400, 0x1401, 0x1404, 0x1405, 0x1410, 0x1411, 0x1414, 0x1415, 
+        0x1440, 0x1441, 0x1444, 0x1445, 0x1450, 0x1451, 0x1454, 0x1455, 
+        0x1500, 0x1501, 0x1504, 0x1505, 0x1510, 0x1511, 0x1514, 0x1515, 
+        0x1540, 0x1541, 0x1544, 0x1545, 0x1550, 0x1551, 0x1554, 0x1555, 
+        0x4000, 0x4001, 0x4004, 0x4005, 0x4010, 0x4011, 0x4014, 0x4015, 
+        0x4040, 0x4041, 0x4044, 0x4045, 0x4050, 0x4051, 0x4054, 0x4055, 
+        0x4100, 0x4101, 0x4104, 0x4105, 0x4110, 0x4111, 0x4114, 0x4115, 
+        0x4140, 0x4141, 0x4144, 0x4145, 0x4150, 0x4151, 0x4154, 0x4155, 
+        0x4400, 0x4401, 0x4404, 0x4405, 0x4410, 0x4411, 0x4414, 0x4415, 
+        0x4440, 0x4441, 0x4444, 0x4445, 0x4450, 0x4451, 0x4454, 0x4455, 
+        0x4500, 0x4501, 0x4504, 0x4505, 0x4510, 0x4511, 0x4514, 0x4515, 
+        0x4540, 0x4541, 0x4544, 0x4545, 0x4550, 0x4551, 0x4554, 0x4555, 
+        0x5000, 0x5001, 0x5004, 0x5005, 0x5010, 0x5011, 0x5014, 0x5015, 
+        0x5040, 0x5041, 0x5044, 0x5045, 0x5050, 0x5051, 0x5054, 0x5055, 
+        0x5100, 0x5101, 0x5104, 0x5105, 0x5110, 0x5111, 0x5114, 0x5115, 
+        0x5140, 0x5141, 0x5144, 0x5145, 0x5150, 0x5151, 0x5154, 0x5155, 
+        0x5400, 0x5401, 0x5404, 0x5405, 0x5410, 0x5411, 0x5414, 0x5415, 
+        0x5440, 0x5441, 0x5444, 0x5445, 0x5450, 0x5451, 0x5454, 0x5455, 
+        0x5500, 0x5501, 0x5504, 0x5505, 0x5510, 0x5511, 0x5514, 0x5515, 
+        0x5540, 0x5541, 0x5544, 0x5545, 0x5550, 0x5551, 0x5554, 0x5555
+      };
+
+      uint32_t z = MortonTable256[y >> 8]   << 17 |
+          MortonTable256[x >> 8]   << 16 |
+          MortonTable256[y & 0xFF] <<  1 |
+          MortonTable256[x & 0xFF];
+      return z;
+    }
+
     template<class A, class T,  int Cmp>
     batch_bool<T, A> compare_int_avx512(batch<T, A> const& self, batch<T, A> const& other) {
       using register_type = typename batch_bool<T, A>::register_type;
@@ -57,29 +118,29 @@ namespace xsimd {
         switch(sizeof(T)) {
           case 1: {
             // shifting to take sign into account
-            __mmask64 mask_low0 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x000000FF)) << 24,
+            uint16_t mask_low0 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x000000FF)) << 24,
                                                          (batch<int32_t, A>(other.data) & batch<int32_t, A>(0x000000FF)) << 24,
                                                          Cmp);
-            __mmask64 mask_low1 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x0000FF00)) << 16,
+            uint16_t mask_low1 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x0000FF00)) << 16,
                                                          (batch<int32_t, A>(other.data) & batch<int32_t, A>(0x0000FF00)) << 16,
                                                          Cmp);
-            __mmask64 mask_high0 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x00FF0000)) << 8,
+            uint16_t mask_high0 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x00FF0000)) << 8,
                                                           (batch<int32_t, A>(other.data) & batch<int32_t, A>(0x00FF0000)) << 8,
                                                           Cmp);
-            __mmask64 mask_high1 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0xFF000000)),
+            uint16_t mask_high1 = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0xFF000000)),
                                                           (batch<int32_t, A>(other.data) & batch<int32_t, A>(0xFF000000)),
                                                           Cmp);
-            return static_cast<register_type>(mask_low0 | (mask_low1 << 16) | (mask_high0 << 32) | (mask_high1 << 48));
+            return static_cast<register_type>(morton(morton(mask_low0, mask_low1), morton(mask_high0, mask_high1)));
           }
           case 2: {
             // shifting to take sign into account
-            __mmask64 mask_low = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x0000FFFF)) << 16,
+            uint16_t mask_low = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0x0000FFFF)) << 16,
                                                           (batch<int32_t, A>(other.data) & batch<int32_t, A>(0x0000FFFF)) << 16,
                                                           Cmp);
-            __mmask64 mask_high = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0xFFFF0000)),
+            uint16_t mask_high = _mm512_cmp_epi32_mask((batch<int32_t, A>(self.data) & batch<int32_t, A>(0xFFFF0000)),
                                                           (batch<int32_t, A>(other.data) & batch<int32_t, A>(0xFFFF0000)),
                                                           Cmp);
-            return static_cast<register_type>(mask_low | (mask_high << 32));
+            return static_cast<register_type>(morton(mask_low, mask_high));
           }
           case 4: return (register_type)_mm512_cmp_epi32_mask(self, other, Cmp);
           case 8: return (register_type)_mm512_cmp_epi64_mask(self, other, Cmp);
@@ -88,16 +149,16 @@ namespace xsimd {
       else {
         switch(sizeof(T)) {
           case 1: {
-            __mmask64 mask_low0 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x00000FF), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x000000FF), Cmp);
-            __mmask64 mask_low1 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x0000FF00), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x0000FF00), Cmp);
-            __mmask64 mask_high0 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x00FF0000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x00FF0000), Cmp);
-            __mmask64 mask_high1 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0xFF000000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0xFF000000), Cmp);
-            return (register_type)(mask_low0 | (mask_low1 << 16) | (mask_high0 << 32) | (mask_high1 << 48));
+            uint16_t mask_low0 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x000000FF), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x000000FF), Cmp);
+            uint16_t mask_low1 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x0000FF00), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x0000FF00), Cmp);
+            uint16_t mask_high0 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x00FF0000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x00FF0000), Cmp);
+            uint16_t mask_high1 = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0xFF000000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0xFF000000), Cmp);
+            return static_cast<register_type>(morton(morton(mask_low0, mask_low1), morton(mask_high0, mask_high1)));
           }
           case 2: {
-            __mmask64 mask_low = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x000FFFF), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x0000FFFF), Cmp);
-            __mmask64 mask_high = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0xFFFF0000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0xFFFF0000), Cmp);
-            return (register_type)(mask_low | (mask_high << 32));
+            uint16_t mask_low = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0x0000FFFF), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0x0000FFFF), Cmp);
+            uint16_t mask_high = _mm512_cmp_epu32_mask(batch<uint32_t, A>(self.data) & batch<uint32_t, A>(0xFFFF0000), batch<uint32_t, A>(other.data)  & batch<uint32_t, A>(0xFFFF0000), Cmp);
+            return static_cast<register_type>(morton(mask_low, mask_high));
           }
           case 4: return (register_type)_mm512_cmp_epu32_mask(self, other, Cmp);
           case 8: return (register_type)_mm512_cmp_epu64_mask(self, other, Cmp);
