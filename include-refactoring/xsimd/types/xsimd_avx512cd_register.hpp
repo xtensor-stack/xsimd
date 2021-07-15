98a348fe741f2439d82c7561f4759a73895c9afc
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-07-15 14:50:07 +0200

    avx512++

---
+++ include-refactoring/xsimd/types/xsimd_avx512cd_register.hpp
@@ -0,0 +1,27 @@
+#ifndef XSIMD_AVX512CD_REGISTER_HPP
+#define XSIMD_AVX512CD_REGISTER_HPP
+
+#include "./xsimd_avx512cd_register.hpp"
+
+namespace xsimd {
+
+  struct avx512cd : avx512f {
+    static constexpr bool supported() { return XSIMD_WITH_AVX512BW; }
+    static constexpr bool available() { return true; }
+    static constexpr unsigned version() { return generic::version(3, 2, 0); }
+  };
+
+#if XSIMD_WITH_AVX512BW
+
+namespace types {
+template <class T> struct get_bool_simd_register<T, avx512cd> {
+  using type = simd_avx512_bool_register<T>;
+};
+
+    XSIMD_DECLARE_SIMD_REGISTER_ALIAS(avx512cd, avx512f);
+
+  }
+#endif
+}
+#endif
+
