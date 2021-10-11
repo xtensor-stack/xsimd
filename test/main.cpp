1ede801afac77755df844d090c7b44f2d3fd52d2
Johan Mabille <johan.mabille@gmail.com>
2021-10-11 11:03:36 +0200

    Added copyright (#594)

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,7 @@
 * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
 * Martin Renou                                                             *
 * Copyright (c) QuantStack                                                 *
+* Copyright (c) Serge Guelton                                              *
 *                                                                          *
 * Distributed under the terms of the BSD 3-Clause License.                 *
 *                                                                          *
