993d309561544f726a2992acbc5328d6544c3657
Tom Nixon <tom@tomn.co.uk>
2021-11-04 18:18:12 +0000

    add test for selected architecture

--- test/test_arch.cpp
+++ test/test_arch.cpp
@@ -70,6 +70,12 @@ struct sum
     }
 };
 
+struct get_arch_version
+{
+    template <class Arch>
+    unsigned operator()(Arch) { return Arch::version(); }
+};
+
 TEST(arch, dispatcher)
 {
     float data[17] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f, 17.f };
@@ -88,6 +94,16 @@ TEST(arch, dispatcher)
         auto dispatched = xsimd::dispatch<sum, xsimd::arch_list<xsimd::avx, xsimd::sse2>>(sum {});
         float res = dispatched(data, 17);
         EXPECT_EQ(ref, res);
+    }
+
+    // check that we pick the most appropriate version
+    {
+        auto dispatched = xsimd::dispatch<get_arch_version,
+                                          xsimd::arch_list<xsimd::sse3, xsimd::sse2>>(get_arch_version {});
+        unsigned expected = xsimd::available_architectures().best >= xsimd::sse3::version()
+            ? xsimd::sse3::version()
+            : xsimd::sse2::version();
+        EXPECT_EQ(expected, dispatched());
     }
 #endif
 }
