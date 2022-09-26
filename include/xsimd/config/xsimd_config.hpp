122f2820faf81c02314c5895d2327c57da2c2e14
Johan Mabille <johan.mabille@gmail.com>
2022-09-26 15:02:36 +0200

    Release 10.0.0rc0

--- include/xsimd/config/xsimd_config.hpp
+++ include/xsimd/config/xsimd_config.hpp
@@ -12,9 +12,9 @@
 #ifndef XSIMD_CONFIG_HPP
 #define XSIMD_CONFIG_HPP
 
-#define XSIMD_VERSION_MAJOR 9
+#define XSIMD_VERSION_MAJOR 10
 #define XSIMD_VERSION_MINOR 0
-#define XSIMD_VERSION_PATCH 1
+#define XSIMD_VERSION_PATCH 0rc0
 
 /**
  * high level free functions
