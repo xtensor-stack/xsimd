4b3f9ffa67d43ce2920c367d9fd31614ac8b90ba
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-04-25 20:05:52 +0200

    Introduce XSIMD_DEFAULT_ARCH as a way to customize the default architecture
    One of the use-case is to detect when the default architecture is used
    by #defining XSIMD_DEFAULT_ARCH xsimd::unsupported.

---
+++ test/test_custom_default_arch.cpp
@@ -0,0 +1,26 @@
+/***************************************************************************
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
+#ifdef __STSE2__
+
+#define XSIMD_DEFAULT_ARCH xsimd::sse2
+#include "xsimd/xsimd.hpp"
+
+#include "test_utils.hpp"
+
+// Could be different than sse2 if we compile for other architecture avx
+static_assert(std::is_same<xsimd::default_arch, xsimd::sse2>::value, "default arch correctly hooked");
+
+#else
+
+#define XSIMD_DEFAULT_ARCH xsimd::unsupported
+#include "xsimd/xsimd.hpp"
+
+#endif
