df3b24e837f86f20e3df1dfad5816728f37ff6c3
DerThorsten <derthorstenbeier@gmail.com>
2023-09-18 12:12:32 +0200

    minimal setup for wasm ci

---
+++ test/test_wasm/test_wasm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# this dir
+TEST_WASM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC_DIR=$TEST_WASM_DIR/../..
+
+
+# emsdk
+# git clone https://github.com/emscripten-core/emsdk
+# cd emsdk
+# ./emsdk install latest
+# ./emsdk activate latest
+# source ./emsdk_env.sh
+source ~/src/emsdk/emsdk_env.sh
+
+
+
+# build wasm
+mkdir -p build
+cd build
+emcmake cmake -DBUILD_TESTS=ON $SRC_DIR
+emmake make -j4
+cd ..
+
+python $TEST_WASM_DIR/test_wasm_playwright.py  build/test
