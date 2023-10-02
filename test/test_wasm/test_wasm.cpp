93a1a569e4b4c0110eff85235f52460714ae21e9
Johan Mabille <johan.mabille@gmail.com>
2023-10-02 22:18:22 +0200

    Applied clang-format

--- test/test_wasm/test_wasm.cpp
+++ test/test_wasm/test_wasm.cpp
@@ -1,34 +1,33 @@
-#include <xmmintrin.h>
-#include <wasm_simd128.h>
 #include "xsimd/xsimd.hpp"
-
+#include <wasm_simd128.h>
+#include <xmmintrin.h>
 
 #include <emscripten/bind.h>
 #include <iostream> // for reporting errors
 
 using namespace emscripten;
 
-
-
 int test_mean()
 {
-    std::cout<<"test_mean"<<std::endl;
-    xsimd::batch<float, xsimd::sse2> a = {1.5, 2.5, 3.5, 4.5};
-    xsimd::batch<float, xsimd::sse2> b = {2.5, 3.5, 4.5, 5.5};
+    std::cout << "test_mean" << std::endl;
+    xsimd::batch<float, xsimd::sse2> a = { 1.5, 2.5, 3.5, 4.5 };
+    xsimd::batch<float, xsimd::sse2> b = { 2.5, 3.5, 4.5, 5.5 };
     auto mean = (a + b) / 2;
     std::cout << mean << std::endl;
     return 0;
 }
 
-
-int run_tests() {
+int run_tests()
+{
     // todo add actual tests
-    if(auto ret = test_mean(); ret != 0) {
+    if (auto ret = test_mean(); ret != 0)
+    {
         return ret;
     }
     return 0;
 }
 
-EMSCRIPTEN_BINDINGS(my_module) {
+EMSCRIPTEN_BINDINGS(my_module)
+{
     emscripten::function("run_tests", &run_tests);
 }
