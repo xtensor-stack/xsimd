1e0ee223100fc8faab8e86274af497ef748a5800
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-10-08 20:56:43 +0200

    Fix minor warnings
    They clutter the build output for no reason.

--- test/doc/explicit_use_of_an_instruction_set.cpp
+++ test/doc/explicit_use_of_an_instruction_set.cpp
@@ -3,7 +3,7 @@
 
 namespace xs = xsimd;
 
-int main(int argc, char* argv[])
+int main(int, char*[])
 {
     xs::batch<double, xs::avx> a = { 1.5, 2.5, 3.5, 4.5 };
     xs::batch<double, xs::avx> b = { 2.5, 3.5, 4.5, 5.5 };
