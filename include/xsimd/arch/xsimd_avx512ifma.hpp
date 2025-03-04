49bcf64aed77285962352786d6a94e2bef57f8e0
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2025-03-04 20:42:44 +0100

    Fix copy-pasted headers

--- include/xsimd/arch/xsimd_avx512ifma.hpp
+++ include/xsimd/arch/xsimd_avx512ifma.hpp
@@ -9,12 +9,12 @@
  * The full license is in the file LICENSE, distributed with this software. *
  ****************************************************************************/
 
-#ifndef XSIMD_AVX512VBMI_HPP
-#define XSIMD_AVX512VBMI_HPP
+#ifndef XSIMD_AVX512IFMA_HPP
+#define XSIMD_AVX512IFMA_HPP
 
 #include <array>
 #include <type_traits>
 
-#include "../types/xsimd_avx512vbmi_register.hpp"
+#include "../types/xsimd_avx512ifma_register.hpp"
 
 #endif
