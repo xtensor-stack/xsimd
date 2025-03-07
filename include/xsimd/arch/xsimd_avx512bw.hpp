140a3d768515cc0103bf63b2fa4093d02b33011e
liangbin <JunMa@linux.alibaba.com>
2025-03-07 20:29:31 +0800

    Fix mismatched requires_arch

--- include/xsimd/arch/xsimd_avx512bw.hpp
+++ include/xsimd/arch/xsimd_avx512bw.hpp
@@ -367,7 +367,7 @@ namespace xsimd
         template <size_t N, class A>
         XSIMD_INLINE batch<int16_t, A> rotate_left(batch<int16_t, A> const& self, requires_arch<avx512bw>) noexcept
         {
-            return bitwise_cast<int16_t>(rotate_left<N, A>(bitwise_cast<uint16_t>(self), avx2 {}));
+            return bitwise_cast<int16_t>(rotate_left<N, A>(bitwise_cast<uint16_t>(self), avx512bw {}));
         }
 
         // sadd
