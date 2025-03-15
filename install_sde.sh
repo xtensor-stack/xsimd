5421abad4fd062f964f2dd0f1ba28d7539078679
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2025-03-15 10:24:58 +0100

    [ci] Upgrade sde version and emulator used
    ---------
    
    Co-authored-by: Johan Mabille <johan.mabille@gmail.com>

--- install_sde.sh
+++ install_sde.sh
@@ -4,7 +4,7 @@
 #python ./intel-sde-downloader.py sde-external-8.35.0-2019-03-11-lin.tar.bz2
 #wget http://software.intel.com/content/dam/develop/external/us/en/protected/sde-external-8.50.0-2020-03-26-lin.tar.bz2
 
-wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36" https://github.com/xtensor-stack/xsimd-testing-resources/releases/download/1.0.0/sde-external-8.69.1-2021-07-18-lin.tar.bz2
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36" https://github.com/xtensor-stack/xsimd-testing-resources/releases/download/2.0.0/sde-external-9.48.0-2024-11-25-lin.tar.xz
 
-tar xvf sde-external-8.69.1-2021-07-18-lin.tar.bz2
+tar xvf sde-external-9.48.0-2024-11-25-lin.tar.xz
 sudo sh -c "echo 0 > /proc/sys/kernel/yama/ptrace_scope"
