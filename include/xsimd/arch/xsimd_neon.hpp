bbe5725b60015e8c34b4c70571179b223861a242
serge-sans-paille <sergesanspaille@free.fr>
2025-07-10 11:41:24 +0200

    eq-bool

--- include/xsimd/arch/xsimd_neon.hpp
+++ include/xsimd/arch/xsimd_neon.hpp
@@ -920,7 +920,7 @@ namespace xsimd
         template <class A, class T, detail::enable_sized_unsigned_t<T, 8> = 0>
         XSIMD_INLINE batch_bool<T, A> eq(batch<T, A> const& lhs, batch<T, A> const& rhs, requires_arch<neon>) noexcept
         {
-            auto eq32 = vceqq_u32(vreinterpretq_u64_u32(lhs), vreinterpretq_u64_u32(rhs));
+            auto eq32 = vceqq_u32(vreinterpretq_u64_u32(lhs.data), vreinterpretq_u64_u32(rhs.data));
             auto rev32 = vrev64q_u32(eq32);
             auto eq64 = vandq_u32(eq32, rev32);
             return batch_bool<T, A>(vreinterpretq_u32_u64(eq64));
@@ -929,7 +929,7 @@ namespace xsimd
         template <class A, class T, detail::enable_sized_signed_t<T, 8> = 0>
         XSIMD_INLINE batch_bool<T, A> eq(batch<T, A> const& lhs, batch<T, A> const& rhs, requires_arch<neon>) noexcept
         {
-            auto eq32 = vceqq_u32(vreinterpretq_s64_u32(lhs), vreinterpretq_s64_u32(rhs));
+            auto eq32 = vceqq_u32(vreinterpretq_s64_u32(lhs.data), vreinterpretq_s64_u32(rhs.data));
             auto rev32 = vrev64q_u32(eq32);
             auto eq64 = vandq_u32(eq32, rev32);
             return batch_bool<T, A>(vreinterpretq_u32_s64(eq64));
