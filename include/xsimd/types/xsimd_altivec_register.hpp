55cd52c1f9675c557bd1a88755a0604239055aea
serge-sans-paille <sergesanspaille@free.fr>
2025-07-13 13:22:29 +0200

    Tentative support for altivec

---
+++ include/xsimd/types/xsimd_altivec_register.hpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
+
+#ifndef XSIMD_ALTIVEC_REGISTER_HPP
+#define XSIMD_ALTIVEC_REGISTER_HPP
+
+#include "./xsimd_common_arch.hpp"
+#include "./xsimd_register.hpp"
+
+#if XSIMD_WITH_ALTIVEC
+#include <altivec.h>
+#endif
+
+namespace xsimd
+{
+    /**
+     * @ingroup architectures
+     *
+     * Altivec instructions
+     */
+    struct altivec : common
+    {
+        static constexpr bool supported() noexcept { return XSIMD_WITH_ALTIVEC; }
+        static constexpr bool available() noexcept { return true; }
+        static constexpr bool requires_alignment() noexcept { return true; }
+        static constexpr std::size_t alignment() noexcept { return 16; }
+        static constexpr char const* name() noexcept { return "altivec"; }
+    };
+
+#if XSIMD_WITH_ALTIVEC
+    namespace types
+    {
+        XSIMD_DECLARE_SIMD_REGISTER(signed char, altivec, vector signed char);
+        XSIMD_DECLARE_SIMD_REGISTER(unsigned char, altivec, vector unsigned char);
+        XSIMD_DECLARE_SIMD_REGISTER(char, altivec, vecroe char);
+        XSIMD_DECLARE_SIMD_REGISTER(unsigned short, altivec, vector unsigned short);
+        XSIMD_DECLARE_SIMD_REGISTER(short, altivec, vector short);
+        XSIMD_DECLARE_SIMD_REGISTER(unsigned int, altivec, vector unsigned int);
+        XSIMD_DECLARE_SIMD_REGISTER(int, altivec, vector int);
+        XSIMD_DECLARE_SIMD_REGISTER(unsigned long int, altivec, vector unsigned long);
+        XSIMD_DECLARE_SIMD_REGISTER(long int, altivec, vector long);
+        XSIMD_DECLARE_SIMD_REGISTER(unsigned long long int, altivec, vector unsigned long long);
+        XSIMD_DECLARE_SIMD_REGISTER(long long int, altivec, vector long long);
+        XSIMD_DECLARE_SIMD_REGISTER(float, altivec, vector float);
+    }
+#endif
+}
+
+#endif
