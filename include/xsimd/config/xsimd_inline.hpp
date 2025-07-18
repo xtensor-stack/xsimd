a2b1f99b8f6df69f4a06a30dc8709d5acb13d377
Clément Garnier <clementgarnier1999@gmail.com>
2025-07-18 22:25:43 +0200

    Fix build with Clang on Windows

--- include/xsimd/config/xsimd_inline.hpp
+++ include/xsimd/config/xsimd_inline.hpp
@@ -12,8 +12,10 @@
 #ifndef XSIMD_INLINE_HPP
 #define XSIMD_INLINE_HPP
 
-#if defined(__GNUC__)
+#if defined __has_attribute
+#if __has_attribute(always_inline)
 #define XSIMD_INLINE inline __attribute__((always_inline))
+#endif
 #elif defined(_MSC_VER)
 #define XSIMD_INLINE inline __forceinline
 #else
