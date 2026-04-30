8df8da7d9ce069348994179f840e3f1680c0dbef
Andreas Krebbel <krebbel@linux.ibm.com>
2026-04-30 10:13:59 +0000

    Fix VSX clang build

--- include/xsimd/types/xsimd_vsx_register.hpp
+++ include/xsimd/types/xsimd_vsx_register.hpp
@@ -39,7 +39,7 @@ namespace xsimd
     namespace types
     {
 
-#define XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(T, Tb)                  \
+#define XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(T, Tv, Tb)              \
     template <>                                                      \
     struct get_bool_simd_register<T, vsx>                            \
     {                                                                \
@@ -55,19 +55,26 @@ namespace xsimd
             operator register_type() const noexcept { return data; } \
         };                                                           \
     };                                                               \
-    XSIMD_DECLARE_SIMD_REGISTER(T, vsx, __vector T)
+    XSIMD_DECLARE_SIMD_REGISTER(T, vsx, __vector Tv)
 
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(signed char, char);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned char, char);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(char, char);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned short, short);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(short, short);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned int, int);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(int, int);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned long, long);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(long, long);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(float, int);
-        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(double, long);
+        // The VSX vector intrinsics do not support long, unsigned long,
+        // and char data types.  batches of these types are vectors of
+        // equivalent types.
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(signed char, signed char, char);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned char, unsigned char, char);
+#ifdef __CHAR_UNSIGNED__
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(char, unsigned char, char);
+#else
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(char, signed char, char);
+#endif
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned short, unsigned short, short);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(short, short, short);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned int, unsigned int, int);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(int, int, int);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(unsigned long, unsigned long long, long long);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(long, long long, long long);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(float, float, int);
+        XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER(double, double, long long);
 
 #undef XSIMD_DECLARE_SIMD_BOOL_VSX_REGISTER
     }
