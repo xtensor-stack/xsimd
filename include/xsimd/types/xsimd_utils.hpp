5aca9a4000c42af5d96dd57ecf85c92356e0ee38
Johan Mabille <johan.mabille@gmail.com>
2026-07-20 18:46:40 +0200

    Removed backport of void_t

--- include/xsimd/types/xsimd_utils.hpp
+++ include/xsimd/types/xsimd_utils.hpp
@@ -291,22 +291,6 @@ namespace xsimd
         }
     }
 
-    /*********************************
-     * Backport of void_t from C++17 *
-     *********************************/
-
-    namespace detail
-    {
-        template <class... T>
-        struct make_void
-        {
-            using type = void;
-        };
-
-        template <class... T>
-        using void_t = typename make_void<T...>::type;
-    }
-
     /**************************************************
      * Equivalent of void_t but with size_t parameter *
      **************************************************/
