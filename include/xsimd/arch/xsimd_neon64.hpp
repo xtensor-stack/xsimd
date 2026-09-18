a34c6486d12a96b5493041f145b8333174fb8cb5
AntoinePrv <AntoinePrv@users.noreply.github.com>
2026-09-18 15:31:35 +0200

    Clea neon64 load stores

--- include/xsimd/arch/xsimd_neon64.hpp
+++ include/xsimd/arch/xsimd_neon64.hpp
@@ -119,43 +119,6 @@ namespace xsimd
             return vreinterpretq_f64_u64(vandq_u64(arg, vreinterpretq_u64_f64(vdupq_n_f64(1.))));
         }
 
-        /********
-         * load *
-         ********/
-#if defined(__clang__) || defined(__GNUC__)
-#define xsimd_aligned_load(inst, type, expr) inst((type)__builtin_assume_aligned(expr, 16))
-#else
-#define xsimd_aligned_load(inst, type, expr) inst((type)expr)
-#endif
-
-        template <class A>
-        XSIMD_INLINE batch<double, A> load_aligned(double const* src, convert<double>, requires_arch<neon64>) noexcept
-        {
-            return xsimd_aligned_load(vld1q_f64, double*, src);
-        }
-
-        template <class A>
-        XSIMD_INLINE batch<double, A> load_unaligned(double const* src, convert<double>, requires_arch<neon64>) noexcept
-        {
-            return vld1q_f64(src);
-        }
-#undef xsimd_aligned_load
-
-        /*********
-         * store *
-         *********/
-
-        template <class A>
-        XSIMD_INLINE void store_aligned(double* dst, batch<double, A> const& src, requires_arch<neon64>) noexcept
-        {
-            vst1q_f64(dst, src);
-        }
-
-        template <class A>
-        XSIMD_INLINE void store_unaligned(double* dst, batch<double, A> const& src, requires_arch<neon64>) noexcept
-        {
-            return store_aligned<A>(dst, src, A {});
-        }
 
         /****************
          * store_stream *
